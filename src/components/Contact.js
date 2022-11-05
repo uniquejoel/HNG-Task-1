939d52aac3979e0f3cd714615fee8bb639bb6419
uniquejoel <joelsunday683@gmail.com>
2022-11-05 14:21:57 +0100

    contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,51 +1,133 @@
 
 import styled from '@emotion/styled'
 import { TextField, Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { Box, Stack } from '@mui/system'
 import React from 'react'
 
 const StyledBox = styled(Box) ({
-    margin: "20px"
+    margin: "20px",
+    padding: "20px"
+})
+
+const StyledTextfield = styled(TextField) ({
+    marginBottom: '40px',
+    height: '44px'
+})
+
+const Styledstack = styled(Stack) ({
+    
 })
 
 const Contact = () => {
   return (
     
-    <StyledBox
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '348px', height: '44px' },
-      }}
-     
-    >
+    <StyledBox>
         <Typography variant='h3'
             sx={{
                 fontFamily: 'Inter',
                 fontStyle: "normal",
                 fontWeight: "600",
                 fontSize: "36px",
-                lineHeight:" 44px"
+                lineHeight:" 44px",
+                color: "#101828"
             }}
         >
             Contact Me</Typography>
 
-      <div>
-        <TextField
-         
+            <Typography 
+            sx={{
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: "400",
+                fontSize: "20px",
+                lineHeight:" 30px",
+                color: "#475467",
+                marginBottom: "30px"
+            }}
+        >
+            Hi there, contact me to ask me about anything you have in mind.</Typography>
+
+      <Styledstack 
+        sx={{
+            display: "flex",
+            flexDirection: {xs: "column", sm: "column", md: "row", lg: "row", xl: "row"},
+        }}
+      >
+        <Box>
+      <Typography 
+            sx={{
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontSize: "14px",
+                lineHeight:" 20px",
+                color: "#344054"
+            }}
+        >
+            First name</Typography>
+        <StyledTextfield
+            sx={{
+                width: {xs: '100%', sm: '100%', md: '348px', lg: '348px', xl: '348px'},
+                marginRight: '35px'
+            }}
           id="outlined-error"
-         
           placeholder="Hello World"
           helperText="Incorrect entry."
         />
-        <TextField
-         
+        </Box>
+
+        <Box>
+        <Typography 
+            sx={{
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontSize: "14px",
+                lineHeight:" 20px",
+                color: "#344054"
+            }}
+        >
+            Last name</Typography>
+        <StyledTextfield
+         sx={{
+            width: {xs: '100%', sm: '100%', md: '348px', lg: '348px', xl: '348px'}
+        }}
          id="outlined-error"
-        
          placeholder="Hello World"
          helperText="Incorrect entry."
        />
+       </Box>
+      
         
-      </div>
+      </Styledstack>
+
+      <Box>
+        <Typography 
+            sx={{
+                fontFamily: 'Inter',
+                fontStyle: "normal",
+                fontWeight: "500",
+                fontSize: "14px",
+                lineHeight:" 20px",
+                color: "#344054"
+            }}
+        >
+            Email</Typography>
+        <TextField
+         sx={{
+            width: "100%",
+            marginBottom: '35px',
+           
+        }}
+         id="outlined-error"
+         placeholder="youremail@gmail.com"
+         helperText="Incorrect entry."
+       />
+       </Box>
+  
+    
+  
+
     </StyledBox>
   )
 }
